ee6c87d3b338bd3e07ea30355e4050c43bd95b88
Devrath <devrath.dev595@gmail.com>
2024-02-14 21:49:23 +0530

    added

--- Applications/AppOne/clientapp/src/main/java/com/istudio/clientapp/MainActivity.java
+++ Applications/AppOne/clientapp/src/main/java/com/istudio/clientapp/MainActivity.java
@@ -1,18 +1,31 @@
 package com.istudio.clientapp;
 
 import android.content.ComponentName;
+import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.istudio.clientapp.databinding.ActivityMainBinding;
+import com.istudio.mylib.ISimpl;
+
+
 public class MainActivity extends AppCompatActivity implements ServiceConnection {
+
+
+    private ActivityMainBinding binding;
+
+
+    private ISimpl service;
 
     @Override
     public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-
+        service = ISimpl.Stub.asInterface(iBinder);
     }
 
     @Override
@@ -24,15 +37,25 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btn1.setOnClickListener(view -> {
+            try {
+                Log.d("POKEMON" , "res:" + service.add(10, 20));
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        });
+
     }
 
     protected void onStart() {
         super.onStart();
 
-        /*bindService(new Intent("app.mabel.com.testservice.MySimpService”).setPackage(“app.mabel.com.testservice”)
-
-                ,this,BIND_AUTO_CREATE);*/
+        Intent intent = new Intent("com.istudio.aidl_app_one.MySimpService");
+        intent.setPackage("com.istudio.aidl_app_one");
+        bindService(intent, this, BIND_AUTO_CREATE);
 
     }
 }
