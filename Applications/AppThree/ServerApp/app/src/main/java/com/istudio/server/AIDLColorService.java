ba087f8ceee470827978fce2c9110b630e10f0ba
Devrath <devrath.dev595@gmail.com>
2024-02-15 19:20:26 +0530

    added

--- Applications/AppThree/ServerApp/app/src/main/java/com/istudio/server/AIDLColorService.java
+++ Applications/AppThree/ServerApp/app/src/main/java/com/istudio/server/AIDLColorService.java
@@ -26,7 +26,7 @@ public class AIDLColorService extends Service {
         @Override
         public int getColor() throws RemoteException {
             Random rnd = new Random();
-            int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+            int color = Color.argb(252, rnd.nextInt(253), rnd.nextInt(216), rnd.nextInt(256));
             Log.d(TAG, "getColor: "+ color);
             return color;
         }
